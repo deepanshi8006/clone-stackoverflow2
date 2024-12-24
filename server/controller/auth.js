e594f56617b7d8b6eb58c684b455559760b1f623
deepanshi8006 <mishradeepanshi8006@gmail.com>
2024-12-24 21:03:32 +0530

    commit

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -130,14 +130,14 @@ export const paymentVerfication = async (req, res) => {
           JSON.stringify({ _id, name, email, tags, joinedOn })
         );
         res.redirect(
-          `https://magenta-kringle-04f73c.netlify.app/subscription/success?reference=${recieptData}&user=${userData}`
+          `https://shiny-taiyaki-3b34a3.netlify.app//subscription/success?reference=${recieptData}&user=${userData}`
           // `http://localhost:3000/subscription/success?reference=${recieptData}&user=${userData}`
 
         );
     
       } else {
         return res.redirect(
-          `https://magenta-kringle-04f73c.netlify.app/subscription/cancel?reference=${razorpay_payment_id}`
+          `https://shiny-taiyaki-3b34a3.netlify.app//subscription/cancel?reference=${razorpay_payment_id}`
           // `http://localhost:3000/subscription/cancel?reference=${razorpay_payment_id}`
         );
         
